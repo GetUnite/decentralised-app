89801387b747217232aa57684aaf13e5770c4d7e
Leonel Sa <leonel.sa@bytepitch.com>
2022-09-16 13:49:15 +0100

    one last attempt

--- server/index.js
+++ server/index.js
@@ -18,6 +18,6 @@ app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log('server started on port 5000');
 });
