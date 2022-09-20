46b6fb069df2ea318cd613c0c4ef6790ae9989d9
Leonel Sa <leonel.sa@bytepitch.com>
2022-09-20 15:39:36 +0100

    Main page small visual changes

--- src/app/modernUI/pages/Main/components/Filter.tsx
+++ src/app/modernUI/pages/Main/components/Filter.tsx
@@ -1,17 +1,24 @@
 import { Box, DropButton, Menu, Select, Text, ThemeContext } from 'grommet';
 
-export const Filter = ({ options, value, onChange, ...rest }) => {
+export const Filter = ({ options, value, onChange, style, ...rest }) => {
   return (
     <ThemeContext.Extend
       value={{
         select: {
           options: {
             text: { size: '16px' },
-          }
+          },
         },
       }}
     >
-      <Select plain options={options} value={value} onChange={onChange} {...rest}/>
+      <Select
+        plain
+        options={options}
+        value={value}
+        onChange={onChange}
+        style={{ ...style, backgroundColor: 'transparent' }}
+        {...rest}
+      />
     </ThemeContext.Extend>
   );
 };
