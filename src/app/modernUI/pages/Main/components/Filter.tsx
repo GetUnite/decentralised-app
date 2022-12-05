c1200b220aaefbb1d47d142563c530cf11fbe468
LeonelSa27 <leonel.sa@bytepitch.com>
2022-12-05 12:16:15 +0000

    [FIX] Visual updates

--- src/app/modernUI/pages/Main/components/Filter.tsx
+++ src/app/modernUI/pages/Main/components/Filter.tsx
@@ -15,24 +15,25 @@ export const Filter = ({
   const dividerColor = isLightMode ? '#EBEBEB' : '#999999';
   const backgroundColor = isLightMode ? '#FFFFFF' : '#1D1D1D';
   const textColor = isLightMode ? 'black' : 'white';
-  
+
   return (
     <>
       <DropButton
         id="filter"
         plain
         label={
-          <Box direction="row" gap={icon ? '4px' : '7px'} align="center">
+          <Box direction="row" gap={icon ? '4px' : '7px'} pad='5px 9px 5px 13px' align='center'>
             {icon && <img src={icon} />}
             <Text size="14px">{heading}</Text>
-            {!icon && <Down />}
+            {!icon && <Down size="small" />}
           </Box>
         }
         style={
           buttonStyle
             ? buttonStyle
             : {
-                padding: '8px 13px',
+              
+                height: '32px',
                 borderRadius: '8px',
                 border: '1px solid #CCCCCC',
               }
@@ -43,15 +44,25 @@ export const Filter = ({
             pad={{ horizontal: 'medium', vertical: '8px' }}
             round="8px"
             width="373px"
-            style={{ display: 'block', background: backgroundColor, color: textColor}}
+            style={{
+              display: 'block',
+              background: backgroundColor,
+              color: textColor,
+            }}
           >
             <Box fill>
               <Heading size="18px">{heading}</Heading>
               {children}
             </Box>
-            <Box justify="end" direction="row" fill pad="8px" style={{
-              borderTop: `2px solid ${dividerColor}`,
-            }}>
+            <Box
+              justify="end"
+              direction="row"
+              fill
+              pad="8px"
+              style={{
+                borderTop: `2px solid ${dividerColor}`,
+              }}
+            >
               <Button plain onClick={onClear}>
                 <Text size="13px" color="#808080">
                   Clear all
