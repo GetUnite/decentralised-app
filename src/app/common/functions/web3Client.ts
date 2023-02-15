4898c84820e19c5a3526364cdf4e157a0b9e9548
LeonelSa27 <leonel.sa@bytepitch.com>
2023-02-15 10:19:35 +0000

    Wallet was always fixed

--- src/app/common/functions/web3Client.ts
+++ src/app/common/functions/web3Client.ts
@@ -198,7 +198,7 @@ export const connectToWallet = async (connectOptions?) => {
 
 export const getCurrentWalletAddress = () => {
   // Use this line to force "get" methods for a specific wallet address
-  return '0xeC3E9c6769FF576Da3889071c639A0E488815926';
+  //return '0xeC3E9c6769FF576Da3889071c639A0E488815926';
   return walletAddress;
 };
 
