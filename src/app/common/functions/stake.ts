86cbcd01146dc7d640515cac06beed087958e50f
OuTseries <dealwithit27@gmail.com>
2023-04-20 20:12:42 +0100

    Remove console.log

--- src/app/common/functions/stake.ts
+++ src/app/common/functions/stake.ts
@@ -432,7 +432,6 @@ export const getStakingPendingRewards = async chain => {
     chain,
   );
 
-  console.log(stakerAccruedRewards);
   let pendingRewardsByToken = [];
   for (const pendingRewardsArray of stakerAccruedRewards) {
     for (const pendingRewards of pendingRewardsArray) {
