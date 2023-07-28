71fe2bc886fab45a65c095ca30968aa69978ca4f
pentatonictritones <pentatonictritones@icloud.com>
2023-07-28 14:55:22 +0700

    fix mount issue

--- src/app/modernUI/pages/StrategyDashboard/StrategyDashboard.tsx
+++ src/app/modernUI/pages/StrategyDashboard/StrategyDashboard.tsx
@@ -15,46 +15,58 @@ export const StrategyDashboard = ({ ...rest }) => {
 
     console.log(getBufferAmountForAssetPolygon(0))
     useEffect(() => {
+        let isMounted = true;  // add this line
+        const updateUSDInfo = async () => {
+            let polygonBuffer = await getBufferAmountForAssetPolygon(0)
+            let polygonBufferInfo = { name: "Polygon Buffer", value: polygonBuffer }
+            let otherSTuff = await getAllLiquidityDirectionInformation(0);
+            let legacyDirection = await getLegacyLiquidityDirectionValue(0);
+            let tempData = [polygonBufferInfo, ...legacyDirection, ...otherSTuff]
+            if (isMounted) {
+                setUsdData(tempData)
+            }
+        }
+
+        const updateEURInfo = async () => {
+            let polygonBuffer = await getBufferAmountForAssetPolygon(1)
+            let polygonBufferInfo = { name: "Polygon Buffer", value: polygonBuffer }
+            let otherSTuff = await getAllLiquidityDirectionInformation(1);
+            let legacyDirection = await getLegacyLiquidityDirectionValue(1);
+            let tempData = [polygonBufferInfo, ...legacyDirection, ...otherSTuff]
+            if (isMounted) {
+                setEurData(tempData)
+            }
+        }
+
+        const updateETHInfo = async () => {
+            let polygonBuffer = await getBufferAmountForAssetPolygon(2)
+            let polygonBufferInfo = { name: "Polygon Buffer", value: polygonBuffer }
+            let otherSTuff = await getAllLiquidityDirectionInformation(2);
+            let legacyDirection = await getLegacyLiquidityDirectionValue(2);
+            let tempData = [polygonBufferInfo, ...legacyDirection, ...otherSTuff]
+            if (isMounted) {
+                setEthData(tempData)
+            }
+        }
+
+        const updateBTCInfo = async () => {
+            let polygonBuffer = await getBufferAmountForAssetPolygon(3)
+            let polygonBufferInfo = { name: "Polygon Buffer", value: polygonBuffer }
+            let otherSTuff = await getAllLiquidityDirectionInformation(3);
+            let legacyDirection = await getLegacyLiquidityDirectionValue(3);
+            let tempData = [polygonBufferInfo, ...legacyDirection, ...otherSTuff]
+            if (isMounted) {
+                setBtcData(tempData)
+            }
+        }
         updateUSDInfo();
         updateEURInfo();
         updateETHInfo();
         updateBTCInfo();
+
+        return () => { isMounted = false }
     }, []);
-    const updateUSDInfo = async () => {
-        let polygonBuffer = await getBufferAmountForAssetPolygon(0)
-        let polygonBufferInfo = { name: "Polygon Buffer", value: polygonBuffer }
-        let otherSTuff = await getAllLiquidityDirectionInformation(0);
-        let legacyDirection = await getLegacyLiquidityDirectionValue(0);
-        let tempData = [polygonBufferInfo, ...legacyDirection, ...otherSTuff]
-        setUsdData(tempData)
-    }
 
-    const updateEURInfo = async () => {
-        let polygonBuffer = await getBufferAmountForAssetPolygon(1)
-        let polygonBufferInfo = { name: "Polygon Buffer", value: polygonBuffer }
-        let otherSTuff = await getAllLiquidityDirectionInformation(1);
-        let legacyDirection = await getLegacyLiquidityDirectionValue(1);
-        let tempData = [polygonBufferInfo, ...legacyDirection, ...otherSTuff]
-        setEurData(tempData)
-    }
-
-    const updateETHInfo = async () => {
-        let polygonBuffer = await getBufferAmountForAssetPolygon(2)
-        let polygonBufferInfo = { name: "Polygon Buffer", value: polygonBuffer }
-        let otherSTuff = await getAllLiquidityDirectionInformation(2);
-        let legacyDirection = await getLegacyLiquidityDirectionValue(2);
-        let tempData = [polygonBufferInfo, ...legacyDirection, ...otherSTuff]
-        setEthData(tempData)
-    }
-
-    const updateBTCInfo = async () => {
-        let polygonBuffer = await getBufferAmountForAssetPolygon(3)
-        let polygonBufferInfo = { name: "Polygon Buffer", value: polygonBuffer }
-        let otherSTuff = await getAllLiquidityDirectionInformation(3);
-        let legacyDirection = await getLegacyLiquidityDirectionValue(3);
-        let tempData = [polygonBufferInfo, ...legacyDirection, ...otherSTuff]
-        setBtcData(tempData)
-    }
 
     return (
         <ResponsiveContext.Consumer>
